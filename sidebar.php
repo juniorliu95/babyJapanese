ee72046839062931f064cc1d54201a0dd76356f3
刘君宇 <liujunyu@MacBook-Air.local>
2022-07-24 23:36:47 +0900

    made the counter

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<!-- Sidebar -->
+<nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" id="mySidebar">
+    <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
+      <i class="fa fa-remove"></i>
+    </a>
+    <h4 class="w3-bar-item"><b>Menu</b></h4>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+  </nav>
